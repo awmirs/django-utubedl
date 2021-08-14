7d928c596baed849c31f71bf0bf7867c2a00ea3f
amir <amirhoseinkamali93@gmail.com>
2021-08-14 22:38:28 +0430

    Basic stuff ready, from now on features can be added

--- download/views.py
+++ download/views.py
@@ -1,6 +1,8 @@
 # Create your views here.
 
 # importing all the required modules
+import sys
+import threading
 from pathlib import Path
 
 from django.shortcuts import render
@@ -10,17 +12,30 @@ from pytube import *
 # defining function
 def youtube(request):
     # checking whether request.method is post or not
-    if request.method == 'POST':
-        # getting link from frontend
-        link = request.POST['link']
-        video = YouTube(link)
+    def x():
+        if request.method == 'POST':
+            # getting link from frontend
+            link = request.POST['link']
 
-        # setting video resolution
-        stream = video.streams.get_lowest_resolution()
+            def completed():
+                print('Download Complete!')
 
-        # downloads video
-        stream.download(output_path=str(Path.home() / "Downloads"))
+            video = YouTube(link)
 
-        # returning HTML page
-        return render(request, 'home.html')
+            # setting video resolution
+            stream = video.streams.get_highest_resolution()
+
+            def downloading(streaming, chunk, bytes_remaining):
+                print(stream.title, ': ', str(round((1 - bytes_remaining / streaming.filesize) * 100, 3)), '% done...')
+
+            video.register_on_progress_callback(downloading)
+
+            # downloads video
+            stream.download(filename=video.title + '.mp4', output_path=str(Path.home() / 'Downloads/Video'))
+
+            # returning HTML page
+            return render(request, 'home.html')
+
+    y = threading.Thread(target=x)
+    y.start()
     return render(request, 'home.html')
