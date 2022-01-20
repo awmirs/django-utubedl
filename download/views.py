8ed551199f000fa13feac57a26069acd3aa3105e
ａｍｉｒ <58553015+awmirs@users.noreply.github.com>
2022-01-20 21:13:19 +0330

    Working on progress bar feature, but at the moment pytube has some bugs and throws RegexException when calling download function

--- download/views.py
+++ download/views.py
@@ -1,9 +1,10 @@
 # Create your views here.
 
 # importing all the required modules
-from datetime import timedelta
 import threading
+from datetime import timedelta
 from pathlib import Path
+from time import sleep
 
 from django.shortcuts import render
 from pytube import *
@@ -16,21 +17,36 @@ def information(request):
     if request.method == 'POST':
         # getting link from frontend
         link = request.POST['link']
-
         # create YouTube object with the provided link
         video = YouTube(link)
-
-        # setting video resolution
-        # stream = video.streams.get_highest_resolution()
-
         title = video.title
         thumbnail = video.thumbnail_url
+        # setting video resolution
+        # stream = video.streams.get_highest_resolution()
         # video file size in Megabytes
         try:
             size = str(round(video.streams.get_highest_resolution().filesize / 1000000, 2)) + ' MB'
         except:
             size = 'An error occurred'
         length = str(timedelta(seconds=video.length))
+        context = {
+            'link': link,
+            'title': title,
+            'thumbnail': thumbnail,
+            'size': size,
+            'length': length
+        }
+        video.streams.get_highest_resolution().download()
+
+        def progress(stream, _chunk, _file_handle, bytes_remaining):
+            current = ((stream.filesize - bytes_remaining) / stream.filesize)
+            percent = ('{0:.1f}').format(current * 100)
+            # progress = int(50 * current)
+            context['percentage'] = percent
+
+        video.register_on_progress_callback(progress)
+
+
         # def downloading(streaming, chunk, bytes_remaining):
         #     print(stream.title, ': ', str(round((1 - bytes_remaining / streaming.filesize) * 100, 3)), '% done...')
         #
@@ -39,14 +55,8 @@ def information(request):
         # # downloads video
         # stream.download(filename=video.title + '.mp4', output_path=str(Path.home() / 'Downloads/Video'))
 
-        context = {
-            'link': link,
-            'title': title,
-            'thumbnail': thumbnail,
-            'size': size,
-            'length': length
-        }
-        return render(request, 'information.html', context)
+
+        return render(request, 'download.html', context)
 
         # returning HTML page
     return render(request, 'information.html')
@@ -61,9 +71,5 @@ def get_link(request):
 
 
 def download(request):
-    if request.method == 'POST':
-        link = request.POST['link']
-        video = YouTube(link)
-        video.streams.get_highest_resolution().download(filename=video.title + '.mp4', output_path=f"{Path.home()}/Downloads/Video")
-        return render(request, 'download.html')
+
     return render(request, 'download.html')
